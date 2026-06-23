c2c6dde07bbc92f35c160ea13890b0ced2e542f2
Richarlison-Avila <richarlisonangeloavila@gmail.com>
2026-06-23 15:38:25 -0300

    Configura executavel e icone do SIGLA

--- sigla-interface/src/main/java/br/com/sigla/interfacegrafica/aplicativo/AplicacaoDesktopSigla.java
+++ sigla-interface/src/main/java/br/com/sigla/interfacegrafica/aplicativo/AplicacaoDesktopSigla.java
@@ -2,6 +2,7 @@ package br.com.sigla.interfacegrafica.aplicativo;
 
 import br.com.sigla.interfacegrafica.inicializacao.AplicacaoSpringSigla;
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -20,6 +21,11 @@ public class AplicacaoDesktopSigla extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("S.I.G.L.A");
+
+        stage.getIcons().add(new Image(
+                AplicacaoDesktopSigla.class.getResourceAsStream("/imagens/sigla.png")
+        ));
+
         FluxoAplicacao fluxoAplicacao = context.getBean(FluxoAplicacao.class);
         fluxoAplicacao.attachStage(stage);
         fluxoAplicacao.showLogin();
